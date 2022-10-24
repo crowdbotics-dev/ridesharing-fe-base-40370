fc834efbfa15da0c4e61fa6b0e4272eda951fb8e
Crowdbotics <engineering@crowdbotics.com>
2022-10-24 17:13:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ridesharing_fe_base_40370",
+  url: "https://ridesharing_fe_base_40370.botics.co",
+  api: "https://ridesharing_fe_base_40370.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
